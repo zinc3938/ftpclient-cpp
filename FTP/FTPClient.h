eac058590033bc6b97572588a99e1432e7f735cb
zinc3938 <zinc3938@gmail.com>
2025-05-09 16:04:16 +0800

    修改linux判断和appendfile（改appe为rest）

--- FTP/FTPClient.h
+++ FTP/FTPClient.h
@@ -19,7 +19,7 @@
 #include <cstdlib>
 #include <cstring>  // strerror, strlen, memcpy, strcpy
 #include <ctime>
-#ifndef LINUX
+#ifndef __linux__
 #include <direct.h>  // mkdir
 #endif
 #include <stdarg.h>  // va_start, etc.
